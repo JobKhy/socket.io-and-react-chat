2b4564f845287bbd576ddf523320b5ea6fd2b49b
Job-Duny <gpinam2000@alumno.ipn.mx>
2022-11-09 00:38:20 -0600

    host config

--- Server/index.js
+++ Server/index.js
@@ -4,8 +4,12 @@ import { Server as SocketServer} from "socket.io";
 import http from "http";
 import cors from "cors";
 import { PORT } from "./config.js";
+import {dirname, join } from "path";
+import {fileURLToPath} from "url";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
@@ -31,6 +35,8 @@ io.on("connection", (socket) => {
     //     console.log("Client disconnected");
     // });
 });
+
+app.use(express.static(join(__dirname, "../client/dist")));
 
 server.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
